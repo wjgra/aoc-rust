3d629dfa62e3a2bf9ab470941972b62583c7ddf3
William John Grace <25494659+wjgra@users.noreply.github.com>
2024-12-11 22:34:14 +0000

    Clean up redundant code in day 9

--- src/day_9_2024.rs
+++ src/day_9_2024.rs
@@ -1,12 +1,8 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::process::id;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let input_path = if args.len() < 2 { "input/day_9_example.txt"} else { &args[1] };
-
+fn get_input(input_path: &str) -> Vec<i32> {
     println!("Loading input at {input_path}");
     let input = File::open(input_path)
         .expect("Failed to open file");
@@ -15,10 +11,24 @@ fn main() {
         .map(|l| l.unwrap())
         .collect();
 
-    let mut input: Vec<i32> = input[0].chars().map(|n| n.to_digit(10).unwrap() as i32).collect();
+    return input[0].chars().map(|n| n.to_digit(10).unwrap() as i32).collect()
+}
 
-    
+fn add_block_to_checksum(checksum: &mut usize, checksum_idx: &mut usize, block_id_number: usize, block_size: usize) {
+    for _ in 0..block_size {
+        *checksum += *checksum_idx * block_id_number;
+        *checksum_idx += 1;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let input_path = if args.len() < 2 { "input/day_9_example.txt"} else { &args[1] };
+    let input = get_input(input_path);
+
     // Part one - move blocks from right to the left
+    let mut working_disk_info = input.clone();
+
     let mut checksum = 0;
     let mut checksum_idx = 0;
 
@@ -30,11 +40,7 @@ fn main() {
         match left % 2 {
             // There is data in leftmost block - add to checksum
             0 => {
-                for _i in 0..input[left] {
-                    let id_number = left / 2;
-                    checksum += checksum_idx * id_number;
-                    checksum_idx += 1;
-                }
+                add_block_to_checksum(&mut checksum, &mut checksum_idx, left / 2, working_disk_info[left] as usize);
                 left += 1; // Move to next block
             },
             // There is no data in leftmost block - fill from right
@@ -43,14 +49,11 @@ fn main() {
                     0 => {
                         let id_number = right / 2;
                         // Rightmost data block completely fills leftmost
-                        if input[right] >= input[left] {
-                            for _i in 0..input[left] {
-                                checksum += checksum_idx * id_number;
-                                checksum_idx += 1;
-                            }
+                        if working_disk_info[right] >= working_disk_info[left] {
+                            add_block_to_checksum(&mut checksum, &mut checksum_idx, id_number, working_disk_info[left] as usize);
 
-                            if input[right] > input[left] {
-                                input[right] -= input[left];
+                            if working_disk_info[right] > working_disk_info[left] {
+                                working_disk_info[right] -= working_disk_info[left];
                             }
                             else {
                                 // We are done with rightmost block
@@ -61,17 +64,13 @@ fn main() {
                         }
                         // Rightmost data block does not completely fill leftmost
                         else {
-                            for _i in 0..input[right] {
-                                checksum += checksum_idx * id_number;
-                                checksum_idx += 1;
-                            }
+                            add_block_to_checksum(&mut checksum, &mut checksum_idx, id_number, working_disk_info[right] as usize);
 
                             // We are not done with leftmost block
-                            input[left] -= input[right];
+                            working_disk_info[left] -= working_disk_info[right];
 
                             right -= 1;
                         }
-                        
                     },
                     // There is no data in the rightmost block - skip it!
                     1 => {
@@ -82,26 +81,13 @@ fn main() {
             },
             _ => unreachable!()
         }
-
-
     }
 
     println!("Part one: {checksum}");
 
-    // Part two - move whole files instead of bytes
-
-    let input = File::open(input_path)
-        .expect("Failed to open file");
-
-    let input: Vec<String> = BufReader::new(input).lines()
-        .map(|l| l.unwrap())
-        .collect();
-
-    let mut input: Vec<i32> = input[0].chars().map(|n| n.to_digit(10).unwrap() as i32).collect();
-
-    let original_input = input.clone();
-    
     // Part one - move blocks from right to the left
+    let mut working_disk_info = input.clone();
+
     let mut checksum = 0;
     let mut checksum_idx = 0;
 
@@ -113,11 +99,7 @@ fn main() {
         match left % 2 {
             // There is data in leftmost block - add to checksum
             0 => {
-                for _i in 0..input[left] {
-                    let id_number = left / 2;
-                    checksum += checksum_idx * id_number;
-                    checksum_idx += 1;
-                }
+                add_block_to_checksum(&mut checksum, &mut checksum_idx, left / 2, working_disk_info[left] as usize);
                 left += 1; // Move to next block
             },
             // There is no data in leftmost block - fill from right
@@ -126,36 +108,32 @@ fn main() {
                     0 => {
                         let id_number = right / 2;
 
-                        // Try each gap from left until it fits - if it doesn't fit in any then 
-                        // just leave in place and calculate checksum
+                        // Try each gap from left until it fits - if it doesn't fit in any then just leave in place and calculate checksum
                         let mut try_left = left;
                         let mut try_checksum_idx = checksum_idx;
                         while try_left < right {
                             // Try to fit data into next free block
-                            if input[try_left] >= input[right] {
+                            if working_disk_info[try_left] >= working_disk_info[right] {
                                 // There is enough space to fit the right block in the left block, but left block may already be partially used
-                                try_checksum_idx += (original_input[try_left] - input[try_left]) as usize;
+                                try_checksum_idx += (input[try_left] - working_disk_info[try_left]) as usize;
 
                                 // Calculate  checksum
-                                for _i in 0..input[right] {
-                                    checksum += try_checksum_idx * id_number;
-                                    try_checksum_idx += 1;
-                                }
+                                add_block_to_checksum(&mut checksum, &mut try_checksum_idx, id_number, working_disk_info[right] as usize);
 
-                                if (input[try_left] == input[right]) && (try_left == left) {
+                                if (working_disk_info[try_left] == working_disk_info[right]) && (try_left == left) {
                                     // We're done with leftmost block
-                                    checksum_idx += original_input[left] as usize;
+                                    checksum_idx += input[left] as usize;
                                     left += 1;
                                 }
                                 else {
-                                    input[try_left] -= original_input[right]; // does this mess up the checksum_idx? consider adding dummy entries
+                                    working_disk_info[try_left] -= input[right];
                                 }
                                 right -= 1;
                                 break;
                             }
                             else {
                                 // Move on to next gap
-                                try_checksum_idx += (original_input[try_left] + original_input[try_left + 1]) as usize; // This is the start of the next free block
+                                try_checksum_idx += (input[try_left] + input[try_left + 1]) as usize; // This is the start of the next free block
                                 try_left += 2; // Next block to try is two along
                             }
                         }
@@ -163,11 +141,8 @@ fn main() {
                         // If block didn't fit anywhere, calculate checksum in place
                         if try_left >= right {
                             // try_checksum_idx currently points to start of gap after rightmost
-                            try_checksum_idx -= input[right] as usize;
-                            for _i in 0..input[right] {
-                                checksum += try_checksum_idx * id_number;
-                                try_checksum_idx += 1;
-                            }
+                            try_checksum_idx -= working_disk_info[right] as usize;
+                            add_block_to_checksum(&mut checksum, &mut try_checksum_idx, id_number, working_disk_info[right] as usize);
                             right -= 1;
                         }           
                     },
@@ -180,10 +155,7 @@ fn main() {
             },
             _ => unreachable!()
         }
-
-
     }
 
     println!("Part two: {checksum}");
-
 }
